dcc2c3dc77d67ef27397c00d0f3b0d319de54779
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-31 20:12:29 +0000

    adds home component

---
+++ src/components/Row.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Row = (title, fetchUrl) => {
+  return (
+    <div>Row</div>
+  )
+}
+
+export default Row
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Request'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        <Main />
+        <Row title='Up Coming' fetchUrl={requests.requestUpcoming}/>
+        <Row title='Popular' fetchUrl={requests.requestPopular}/>
+        <Row title='Trending' fetchUrl={requests.requestTrending}/>
+        <Row title='Top Rated' fetchUrl={requests.requestTopRated}/>
+        <Row title='Horror' fetchUrl={requests.requestHorror}/>
+    </>
   )
 }
 
