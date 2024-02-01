71c4e810803ce7cb23559c992b3201a7c5bdd999
Sidiq Hersi <hersisidiq@gmail.com>
2024-02-01 15:24:31 +0000

    fixes bug and completes movie and row components

--- src/Request.js
+++ src/Request.js
@@ -1,11 +1,11 @@
-const key = 'cf39eb1b5099f2a752ebd5d82f797867'
+const key = "cf39eb1b5099f2a752ebd5d82f797867";
 
 const requests = {
-    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
-    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
-  };
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestNowPlaying:  `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=2`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
 
-  export default requests
+export default requests;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,12 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Row title="UpComing" fetchUrl={requests.requestUpcoming} />
-      <Row title="Popular" fetchUrl={requests.requestPopular} />
-      <Row title="Trending" fetchUrl={requests.requestTrending} />
-      <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
-      <Row title="Horror" fetchUrl={requests.requestHorror} />
+      <Row rowID="1" title="Upcoming" fetchURL={requests.requestUpcoming} />
+      <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowID="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowID="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowID="5" title="Now Playing" fetchURL={requests.requestNowPlaying} />
     </>
   );
 };
-
 export default Home;
