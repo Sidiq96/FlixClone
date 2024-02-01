a5d893edcfdc4aa2ea6cbc24687a63ef5b90dc5e
Sidiq Hersi <hersisidiq@gmail.com>
2024-02-01 13:55:33 +0000

    adds the rows compoent and fixes some bugs

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {FaHeart, FaRegHeart} from 'react-icons/fa'
+import axios from "axios";
 
-const Row = (title, fetchUrl) => {
-  return (
-    <div>Row</div>
-  )
-}
+const Row = ({ title, fetchUrl }) => {
+    const [movies, setMovies] = useState([]);
+    const [like, setLike] = useState(false);
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            if (fetchUrl) {
+              const response = await axios.get(fetchUrl);
+              setMovies(response.data.results);
+            }
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchData();
+      }, [fetchUrl]);
+    return (
+      <>
+        <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+        <div className="relative flex items-center">
+          <div id={"slider"}>
+            {movies.map((item, id) => (
+              <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2" key={id}>
+                <img
+                  className="w-full h-auto block"
+                  src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
+                  alt={item?.title}
+                />
+                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                  <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">{item?.title}</p>
+                  <p>
+                    {like ? <FaHeart  className="absolute top-2 left-2 text-gray-300"/> : <FaRegHeart />}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  };
+  
 
-export default Row
+export default Row;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Main from '../components/Main'
-import Row from '../components/Row'
-import requests from '../Request'
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Request";
 
 const Home = () => {
   return (
     <>
-        <Main />
-        <Row title='Up Coming' fetchUrl={requests.requestUpcoming}/>
-        <Row title='Popular' fetchUrl={requests.requestPopular}/>
-        <Row title='Trending' fetchUrl={requests.requestTrending}/>
-        <Row title='Top Rated' fetchUrl={requests.requestTopRated}/>
-        <Row title='Horror' fetchUrl={requests.requestHorror}/>
+      <Main />
+      <Row title="UpComing" fetchUrl={requests.requestUpcoming} />
+      <Row title="Popular" fetchUrl={requests.requestPopular} />
+      <Row title="Trending" fetchUrl={requests.requestTrending} />
+      <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
+      <Row title="Horror" fetchUrl={requests.requestHorror} />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
